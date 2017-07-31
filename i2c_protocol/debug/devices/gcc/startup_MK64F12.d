90d78cf4d8a2ba93cd71ffaafae24cd399f735b9
cody.nguyen <cody.nguyen@terralogic.com>
2017-07-31 10:58:55 +0700

    Update remove some debug file

--- i2c_protocol/debug/devices/gcc/startup_MK64F12.d
+++
@@ -1,1 +0,0 @@
-devices/gcc/startup_MK64F12.o: ../devices/gcc/startup_MK64F12.S
--- i2c_protocol/debug/startup/startup_MK64F12.d
+++
@@ -1,2 +0,0 @@
-startup/startup_MK64F12.d: \
- H:/My\ Study/Embedded\ Linux/FRDM-K64F/SDK_2.1_MK64FN1M0xxx12/devices/MK64F12/gcc/startup_MK64F12.S
